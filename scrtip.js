5cb784e6379bc104e7270dbae156b2b5de979186
marioseixas <seixas@ib.bsb.br>
2023-12-26 07:34:18 -0300

    Delete scrtip.js

--- scrtip.js
+++
@@ -1,9 +0,0 @@
-const btnmob = document.getElementById('btnmobile');
-
-function togglemenu(){
-    const menu = document.getElementById('men');
-    menu.classList.toggle('active');
-}
-
-
-btnmob.addEventListener('click', togglemenu);
